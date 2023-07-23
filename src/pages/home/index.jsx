3b8217bef333c115ec9545c6fc424ec120c94e6f
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-23 11:21:47 +0700

    fixing jumbotron and adding section title

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,10 @@ import { getNowPlayingMovies } from '../../scripts/data/themoviedb-source';
 import CONFIG from '../../scripts/global/config';
 
 import { RiMovie2Line as IconMovie } from 'react-icons/ri';
+import {
+  MdPlayCircleOutline as IconNowPlayingMovies,
+  MdOutlineUpdate as IconUpcomingMovies
+} from 'react-icons/md';
 
 const HomePage = () => {
   const [movies, setMovies] = useState([]);
@@ -16,7 +20,7 @@ const HomePage = () => {
 
   return (
     <>
-      <div className='h-screen md:h-[50vh] lg:h-screen bg-[url("/images/movie2.webp")] bg-cover bg-center flex justify-center md:justify-normal items-center p-4 md:p-8'>
+      <div className='h-[60vh] md:h-[50vh] lg:h-screen bg-[url("/images/movie2.webp")] bg-cover bg-center flex justify-center md:justify-normal items-center p-4 md:p-8'>
         <div className='flex gap-2'>
           <div className='text-white text-6xl md:text-7xl lg:text-[6.3rem]'>
             <IconMovie/>
@@ -27,14 +31,22 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      <div className='bg-gradient-to-b from-red-600 to-black via-red-800 p-4 md:p-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-8'>
-        {movies.map((movie) => (
-          <div key={movie.id} className='bg-black p-1 md:p-2'>
-            <div className='flex flex-col'>
-              <img src={`${CONFIG.BASE_IMAGE_URL}${movie["poster_path"]}`} alt={movie.title} />
-            </div>
+      <div className='bg-gradient-to-b from-red-600 to-black via-red-800 p-4 md:p-8 flex flex-col gap-4'>
+        <div className='flex items-center gap-2'>
+          <div className='text-white text-3xl'>
+            <IconNowPlayingMovies/>
           </div>
-        ))}
+          <h2 className='font-semibold text-white text-lg md:text-xl lg:text-2xl'>Now Playing</h2>
+        </div>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-8'>
+          {movies.map((movie) => (
+            <div key={movie.id} className='bg-black p-1 md:p-2'>
+              <div className='flex flex-col'>
+                <img src={`${CONFIG.BASE_IMAGE_URL}${movie["poster_path"]}`} alt={movie.title} />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
