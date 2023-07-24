07be6c689485713295520868b19b9c12c556944f
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-24 23:50:29 +0700

    adding search icon in header

--- src/components/fragments/header/index.jsx
+++ src/components/fragments/header/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 
 import { RiMovie2Line as IconMovie } from 'react-icons/ri';
+import { MdSearch as IconSearch } from 'react-icons/md';
 import Icon from '../../elements/icon';
 
 const Header = () => {
   return (
-    <header className='bg-black flex items-center justify-between p-4 md:px-8 lg:px-16'>
-      <Icon fontsize="text-2xl">
-        <IconMovie/>
+    <header className='bg-black flex items-center justify-between fixed top-0 left-0 right-0 z-10 px-4 py-3 md:px-8 lg:px-16'>
+      <h2 className='font-bold text-white text-lg md:text-xl lg:text-2xl'>MOVOSTORE</h2>
+      <Icon fontsize="text-3xl">
+        <IconSearch/>
       </Icon>
-      <div>
-        <input type="text" />
-      </div>
     </header>
   )
 };
