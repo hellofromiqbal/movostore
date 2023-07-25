f98087b866972a844826b05e16d46fb34076494c
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-25 22:09:01 +0700

    add navigate on movieListNoSwipe component

--- src/components/elements/movieListNoSwipe/index.jsx
+++ src/components/elements/movieListNoSwipe/index.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 import { AiFillStar as IconStar } from 'react-icons/ai';
 import { generatePoster, getMovieDetails } from '../../../scripts/data/themoviedb-source';
+import { useNavigate } from 'react-router-dom';
 
 const MovieListNoSwipe = (props) => {
   const { movies } = props;
+
+  const navigate = useNavigate();
+
+  const onCardClick = (id) => {
+    navigate(`/detail/${id}`);
+  };
 
   return (
     <div className='flex'>
@@ -13,7 +20,7 @@ const MovieListNoSwipe = (props) => {
           <div
             key={movie.id}
             className='bg-black p-1 md:p-2 cursor-pointer'
-            onClick={() => getMovieDetails(movie.id, (data) => console.log(data))}
+            onClick={() => onCardClick(movie.id)}
           >
             <div className='flex flex-col'>
               <img src={generatePoster(movie["poster_path"])} alt={movie.title} />
