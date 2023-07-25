8566191f15d7c54cb71d06e2ae4b0a4ab4d84235
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-25 22:17:36 +0700

    fix movie detail image responsiveness

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -35,21 +35,21 @@ const DetailPage = () => {
     >
       <SectionTitle section="detail"/>
       <div className='flex flex-col md:flex-row gap-2 md:gap-6 w-full md:w-6/6 lg:w-5/6 m-auto'>
-        <div className='basis-2/5 bg-black p-1 md:p-2 w-max h-max'>
+        <div className='basis-1/2 lg:basis-2/5 bg-black p-1 md:p-2 h-max'>
           <img src={generatePoster(movie["poster_path"])} alt={movie.title} />
         </div>
-        <div className='basis-3/5 text-white flex flex-col lg:gap-2'>
-          <h2 className='font-semibold text-base md:text-xl lg:text-2xl'>{movie.title}</h2>
-          <cite>{`"${movie.tagline}"`}</cite>
-          <h3>Genres: {movie.genres?.map((genre, i) => wordFormatter(movie, "genres", genre, "name", i))}</h3>
-          <p>Languages: {movie["spoken_languages"]?.map((language, i) => wordFormatter(movie, "spoken_languages", language, "english_name", i))}</p>
-          <p>Release Date: {movie["release_date"]}</p>
-          <p>Rating: {movie["vote_average"]}</p>
-          <p>Duration: {movie.runtime} minutes</p>
-          <p>{movie.overview}</p>
-          <p>Budget: {currencyFormatter.format(movie.budget)}</p>
-          <p>Revenue: {currencyFormatter.format(movie.revenue)}</p>
-          <p>Production Companies: {movie["production_companies"]?.map((company, i) => wordFormatter(movie, "production_companies", company, "name", i))}</p>
+        <div className='basis-1/2 lg:basis-3/5 text-white flex flex-col lg:gap-2 gap-1'>
+          <h2 className='font-semibold text-lg md:text-xl lg:text-2xl'>{movie.title}</h2>
+          <cite className='text-sm md:text-base'>{`"${movie.tagline}"`}</cite>
+          <p className='text-sm md:text-base'>Genres: {movie.genres?.map((genre, i) => wordFormatter(movie, "genres", genre, "name", i))}</p>
+          <p className='text-sm md:text-base'>Languages: {movie["spoken_languages"]?.map((language, i) => wordFormatter(movie, "spoken_languages", language, "english_name", i))}</p>
+          <p className='text-sm md:text-base'>Release Date: {movie["release_date"]}</p>
+          <p className='text-sm md:text-base'>Rating: {movie["vote_average"]}</p>
+          <p className='text-sm md:text-base'>Duration: {movie.runtime} minutes</p>
+          <p className='text-sm md:text-base'>{movie.overview}</p>
+          <p className='text-sm md:text-base'>Budget: {currencyFormatter.format(movie.budget)}</p>
+          <p className='text-sm md:text-base'>Revenue: {currencyFormatter.format(movie.revenue)}</p>
+          <p className='text-sm md:text-base'>Production Companies: {movie["production_companies"]?.map((company, i) => wordFormatter(movie, "production_companies", company, "name", i))}</p>
         </div>
       </div>
     </SectionLayout>
