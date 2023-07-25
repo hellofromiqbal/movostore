e9c67a2925e103f97da7142b07d0d9ff6066928c
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-25 11:54:03 +0700

    add floating search result component

---
+++ src/components/elements/floatingSearchResult/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FloatingSearchResult = () => {
+  return (
+    <div className='absolute bg-white bottom-[-1.8rem] left-0 right-0 rounded-sm px-2'>
+      <h1>FloatingSearchResult</h1>
+    </div>
+  )
+};
+
+export default FloatingSearchResult;
--- src/components/fragments/searchBar/index.jsx
+++ src/components/fragments/searchBar/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Button from '../../elements/button';
 import { FiSearch as IconSearch } from 'react-icons/fi';
+import FloatingSearchResult from '../../elements/floatingSearchResult';
 
 const SearchBar = () => {
   return (
-    <div className='hidden md:flex'>
+    <div className='relative hidden md:flex'>
       <input className='md:w-72 lg:w-96 rounded-s-sm px-2 text-sm md:text-base' type="text" placeholder='Harry Potter' />
       <Button bgcolor="bg-white" textcolor="text-black" fontsize="md:text-xl lg:text-2xl" rounded="rounded-e-sm">
         <IconSearch/>
       </Button>
+      <FloatingSearchResult/>
     </div>
   )
 };
