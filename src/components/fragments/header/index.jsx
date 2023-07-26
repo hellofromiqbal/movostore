670a2d021cd3289a041e88861005245a00233d96
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-26 15:49:17 +0700

    adjust header font size

--- src/components/fragments/header/index.jsx
+++ src/components/fragments/header/index.jsx
@@ -14,13 +14,13 @@ const Header = () => {
   return (
     <header className='bg-black flex items-center justify-between fixed top-0 left-0 right-0 z-10 shadow-md px-4 py-3 md:px-8 lg:px-16'>
       <div className='flex items-center gap-2 cursor-pointer' onClick={() => navigate(`/`)}>
-        <Icon fontsize="text-lg md:text-xl lg:text-2xl">
+        <Icon fontsize="text-sm md:text-base lg:text-xl">
           <IconMovie/>
         </Icon>
-        <h2 className='font-semibold text-white text-sm md:text-xl lg:text-2xl'>MOVOSTORE</h2>
+        <h2 className='font-semibold text-white text-sm md:text-base lg:text-xl'>MOVOSTORE</h2>
       </div>
       <SearchBar/>
-      <Button fontsize="md:hidden text-lg md:text-xl lg:text-2xl">
+      <Button fontsize="md:hidden text-sm md:text-base lg:text-xl">
         <IconSearch/>
       </Button>
     </header>
