08435b5f40a603d0c9f3c654f5505e5f4336df36
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-26 16:34:04 +0700

    adjust floatingSearchResult component

--- src/components/elements/floatingSearchResult/index.jsx
+++ src/components/elements/floatingSearchResult/index.jsx
@@ -9,11 +9,11 @@ const FloatingSearchResult = (props) => {
       {movies.splice(0, 3).map((movie) => (
         <div key={movie.id} className='flex flex-row py-2 border-b-[1px] md:gap-2 cursor-pointer' onClick={() => onCardClick(movie.id)}>
           <div className='basis-1/3 h-40 flex object-cover'>
-            <img src={generatePoster(movie["poster_path"])} alt={movie.title} />
+            <img src={generatePoster(movie["poster_path"])} alt={movie.title}/>
           </div>
           <div className='basis-2/3 h-40 overflow-auto flex flex-col gap-1'>
             <h1 className='font-semibold text-sm md:text-base'>{movie.title}</h1>
-            <p className='opacity-90 text-sm md:text-base'>{movie["release_date"]}</p>
+            <p className='opacity-70 text-sm md:text-base'>{movie["release_date"]}</p>
             <p className='opacity-50 text-sm md:text-base'>{movie.overview}</p>
           </div>
         </div>
