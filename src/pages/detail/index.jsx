1e68eefa4fd1d22b2eb7b84f15a6e160c905b2ac
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-26 12:59:09 +0700

    adjust xl viewport

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -31,7 +31,7 @@ const DetailPage = () => {
       padding={"px-4 pt-16 pb-4 md:px-8 lg:px-16"}
     >
       <SectionTitle section="movieDetails"/>
-      <div className='flex flex-col md:flex-row gap-2 md:gap-6 w-full md:w-6/6 lg:w-5/6 m-auto'>
+      <div className='flex flex-col md:flex-row gap-2 md:gap-6 w-full md:w-6/6 lg:w-5/6 xl:w-4/6 m-auto'>
         <div className='basis-1/2 lg:basis-2/5 bg-black p-1 md:p-2 h-max'>
           <img src={generatePoster(movie["poster_path"])} alt={movie.title} />
         </div>
