5cae51d668b50946d3e38f4a3bfacc558e51cc97
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-27 14:37:41 +0700

    adjust floating search result

--- src/components/elements/floatingSearchResult/index.jsx
+++ src/components/elements/floatingSearchResult/index.jsx
@@ -6,19 +6,23 @@ const FloatingSearchResult = (props) => {
 
   return (
     <div>
-      {movies.splice(0, 3).map((movie) => (
-        <div key={movie.id} className='flex flex-row py-2 border-b-[1px] md:gap-2 cursor-pointer' onClick={() => handleClickSearchResult(movie.id)}>
-          <div className='basis-1/3 h-40 flex object-cover'>
-            <img src={generatePoster(movie["poster_path"])} alt={movie.title}/>
+      <div className='h-96 overflow-auto'>
+        {movies.splice(0, 5).map((movie) => (
+          <div key={movie.id} className='flex flex-row py-2 border-b-[1px] md:gap-2 cursor-pointer' onClick={() => handleClickSearchResult(movie.id)}>
+            <div className='basis-1/3 h-40 flex object-cover'>
+              <img src={generatePoster(movie["poster_path"])} alt={movie.title}/>
+            </div>
+            <div className='basis-2/3 h-40 flex flex-col gap-1'>
+              <h1 className='font-semibold text-sm md:text-base'>{movie.title}</h1>
+              <p className='opacity-70 text-sm md:text-base'>{movie["release_date"]}</p>
+              <p className='opacity-50 text-sm md:text-base overflow-auto'>{movie.overview}</p>
+            </div>
           </div>
-          <div className='basis-2/3 h-40 overflow-auto flex flex-col gap-1'>
-            <h1 className='font-semibold text-sm md:text-base'>{movie.title}</h1>
-            <p className='opacity-70 text-sm md:text-base'>{movie["release_date"]}</p>
-            <p className='opacity-50 text-sm md:text-base'>{movie.overview}</p>
-          </div>
-        </div>
-      ))}
-      <a className="py-2 text-center cursor-pointer" onClick={() => handleClickShowAllResult()}>Show all results</a>
+        ))}
+      </div>
+      <div className='my-2 text-center'>
+        <a className="cursor-pointer text-sm md:text-base" onClick={() => handleClickShowAllResult()}>Show all results</a>
+      </div>
     </div>
   )
 };
