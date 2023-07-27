4c5a45c542f4b5559c4f1312ef3e17aa33abc81b
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-07-27 14:48:57 +0700

    adjust header logo

--- src/components/fragments/header/index.jsx
+++ src/components/fragments/header/index.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import SearchBar from '../searchBar';
 import Button from '../../elements/button';
+import Icon from '../../elements/icon';
 import { FiSearch as IconSearch } from 'react-icons/fi';
 import { AiOutlineClose as IconClose } from 'react-icons/ai';
+import { RiMovie2Line as IconMovie } from 'react-icons/ri';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const [searchIconMobileClicked, setSearchIconMobileClicked] = useState(false);
@@ -11,16 +14,21 @@ const Header = () => {
     setSearchIconMobileClicked((prev) => !prev);
   };
 
+  const navigate = useNavigate();
+
   return (
-    <header className='bg-black h-14 fixed top-0 left-0 right-0 z-10 flex items-center px-4'>
+    <header className='bg-black h-14 fixed top-0 left-0 right-0 z-10 flex items-center px-4 md:px-8 lg:px-16'>
       <div className='w-full md:basis-1/2'>
         {searchIconMobileClicked ?
           <div className='w-full md:w-96'>
             <SearchBar/>
           </div>
           :
-          <div className='text-white'>
-            <h1>Movostore</h1>
+          <div className='text-white flex items-center gap-1 md:gap-2' onClick={() => navigate("/")}>
+            <Icon fontsize="text-2xl">
+              <IconMovie/>
+            </Icon>
+            <h2 className='font-semibold text-sm md:text-base'>MOVOSTORE</h2>
           </div>
         }
       </div>
