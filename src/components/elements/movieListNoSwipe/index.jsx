ebd7a9a19d40d11ede6970416e18af4c6421df9c
Muhammad Iqbal <hellofromiqbal@gmail.com>
2023-08-01 08:43:16 +0700

    add no liked movies display on movieListNoSwipe

--- src/components/elements/movieListNoSwipe/index.jsx
+++ src/components/elements/movieListNoSwipe/index.jsx
@@ -15,28 +15,34 @@ const MovieListNoSwipe = (props) => {
 
   return (
     <div className='flex'>
-      <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6 gap-2 md:gap-3 lg:gap-4 xl:gap-6'>
-        {movies?.map((movie) => (
-          <div
-            key={movie.id}
-            className='bg-black p-1 md:p-2 cursor-pointer'
-            onClick={() => onCardClick(movie.id)}
-          >
-            <div className='flex flex-col overflow-hidden'>
-              <img src={generatePoster(movie["poster_path"])} alt={movie.title} className='hover:scale-[1.1] transition duration-500'/>
-            </div>
-            <div className='md:hidden text-white p-1'>
-              <div className='flex items-center gap-2'>
-                <div className='text-yellow-300'>
-                  <IconStar/>
-                </div>
-                <h4 className='text-white opacity-50 text-sm md:text-base'>{movie["vote_average"]}</h4>
+      {movies.length === 0 ?
+        <div className='text-white h-96 md:h-screen w-full flex justify-center items-center'>
+          <h1 className='text-base md:text-lg'>You don't have any liked movies yet!</h1>
+        </div>
+        :
+        <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6 gap-2 md:gap-3 lg:gap-4 xl:gap-6'>
+          {movies?.map((movie) => (
+            <div
+              key={movie.id}
+              className='bg-black p-1 md:p-2 cursor-pointer'
+              onClick={() => onCardClick(movie.id)}
+            >
+              <div className='flex flex-col overflow-hidden'>
+                <img src={generatePoster(movie["poster_path"])} alt={movie.title} className='hover:scale-[1.1] transition duration-500'/>
               </div>
-              <h3 className='text-sm md:text-base'>{movie.title}</h3>
+              <div className='md:hidden text-white p-1'>
+                <div className='flex items-center gap-2'>
+                  <div className='text-yellow-300'>
+                    <IconStar/>
+                  </div>
+                  <h4 className='text-white opacity-50 text-sm md:text-base'>{movie["vote_average"]}</h4>
+                </div>
+                <h3 className='text-sm md:text-base'>{movie.title}</h3>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      }
     </div>
   )
 };
